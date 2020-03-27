e9b8473368e42e0caa1f7795896d063871b89232
tog22 <tom.ash.github@philosofiles.com>
2020-03-27 18:42:43 -0500

    (Steps 3 & 4 being including container.php & middleware.php (empty to start with)

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -5,11 +5,11 @@ require_once __DIR__ . '/../vendor/autoload.php';
 // Instantiate the app
 $app = new \Slim\App(['settings' => require __DIR__ . '/../config/settings.php']);
 
-// Set up dependencies
-require  __DIR__ . '/container.php';
-
-// Register middleware
-require __DIR__ . '/middleware.php';
+// Set up dependencies 
+require  __DIR__ . '/container.php'; 
+ 
+// Register middleware 
+require __DIR__ . '/middleware.php'; 
 
 // Register routes
 require __DIR__ . '/routes.php';
