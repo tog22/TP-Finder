7aa0d48f455b389e10318618515162c8ef3f9140
tog22 <tom.ash.github@philosofiles.com>
2020-03-27 18:37:08 -0500

    (Actually adding the files from steps 3 & 4)

---
+++ config/container.php
@@ -0,0 +1,13 @@
+<?php
+
+use Slim\Container;
+
+/** @var \Slim\App $app */
+$container = $app->getContainer();
+
+// Activating routes in a subfolder
+$container['environment'] = function () {
+    $scriptName = $_SERVER['SCRIPT_NAME'];
+    $_SERVER['SCRIPT_NAME'] = dirname(dirname($scriptName)) . '/' . basename($scriptName);
+    return new Slim\Http\Environment($_SERVER);
+};
---
+++ config/middleware.php
@@ -0,0 +1,4 @@
+<?php
+
+// Slim middleware
+
