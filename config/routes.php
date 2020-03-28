fb12afc83300e40140e3fa7fd99dd529041fbc7e
tog22 <tom.ash.github@philosofiles.com>
2020-03-27 20:18:03 -0500

    Odan step 9: Add time.twig plus a route for it (/time)

--- config/routes.php
+++ config/routes.php
@@ -3,6 +3,10 @@
 use Slim\Http\Request;
 use Slim\Http\Response;
 
+use Slim\Views\Twig;
+
+
+
 $app->get('/', function (Request $request, Response $response) {
     $response->getBody()->write("Welcome to the home page");
 
@@ -14,4 +18,19 @@ $app->get('/hello/{name}', function (Request $request, Response $response) {
     $response->getBody()->write("Hello, $name");
 
     return $response;
+});
+
+
+/**********
+*         *
+*  /time  *
+*         *
+**********/
+
+$app->get('/time', function (Request $request, Response $response) {
+    $viewData = [
+        'now' => date('Y-m-d H:i:s')
+    ];
+
+    return $this->get(Twig::class)->render($response, 'time.twig', $viewData);
 });
