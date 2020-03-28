16f4ba2246cf6e86e8d7ef27fcc1d9d34a2d0d1b
tog22 <tom.ash.github@philosofiles.com>
2020-03-27 19:47:57 -0500

    Odan step 6: bootstrap.php includes settings.php

---
+++ config/settings.php
@@ -0,0 +1,39 @@
+<?php
+
+$settings = [];
+
+// Slim settings
+$settings['displayErrorDetails'] = true;
+$settings['determineRouteBeforeAppMiddleware'] = true;
+
+// Path settings
+$settings['root'] = dirname(__DIR__);
+$settings['temp'] = $settings['root'] . '/tmp';
+$settings['public'] = $settings['root'] . '/public';
+
+// View settings
+$settings['twig'] = [
+    'path' => $settings['root'] . '/templates',
+    'cache_enabled' => false,
+    'cache_path' =>  $settings['temp'] . '/twig-cache'
+];
+
+// Database settings
+$settings['db'] = [
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'username' => 'tog22_low_sec',
+    'database' => 'tog22_tp_odan',
+    'password' => 'lowsecpw',
+    'charset' => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'flags' => [
+        PDO::ATTR_PERSISTENT => false,
+        // Enable exceptions
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        // Set default fetch mode
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    ],
+];
+
+return $settings;
