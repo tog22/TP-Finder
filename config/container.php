bc581ac65de92c046bc20338fa7647b8c8afab79
tog22 <tom.ash.github@philosofiles.com>
2020-03-27 20:13:51 -0500

    Odan step 8: Register Twig View in container.php

--- config/container.php
+++ config/container.php
@@ -11,3 +11,32 @@ $container['environment'] = function () {
     $_SERVER['SCRIPT_NAME'] = dirname(dirname($scriptName)) . '/' . basename($scriptName);
     return new Slim\Http\Environment($_SERVER);
 };
+
+
+
+/****************
+* ADD TWIG-VIEW *
+****************/
+
+use Slim\Views\Twig;
+
+// Register Twig View helper
+$container[Twig::class] = function (Container $container) {
+    $settings = $container->get('settings');
+    $viewPath = $settings['twig']['path'];
+
+    $twig = new Twig($viewPath, [
+        'cache' => $settings['twig']['cache_enabled'] ? $settings['twig']['cache_path'] : false
+    ]);
+
+    /** @var Twig_Loader_Filesystem $loader */
+    $loader = $twig->getLoader();
+    $loader->addPath($settings['public'], 'public');
+
+    // Instantiate and add Slim specific extension
+    $router = $container->get('router');
+    $uri = \Slim\Http\Uri::createFromEnvironment($container->get('environment'));
+    $twig->addExtension(new \Slim\Views\TwigExtension($router, $uri));
+
+    return $twig;
+};
